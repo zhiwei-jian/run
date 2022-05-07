f3ba85e2730b51a4072f6c35b121621ee3b97600
Z_Jian <Z_Jian@Dell.com>
2022-05-07 09:41:36 +0800

    add API

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { GridDataService } from '../service/grid-data.service';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   title = 'User';
   mySite = 'User';
+  // constructor(private service : GridDataService) { }
   constructor() { }
-
   ngOnInit(): void {
+
   }
 
 }
